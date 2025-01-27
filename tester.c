7bcbffec59cd65d4594884660528fd465316d839
Maksym Floria <floryan2206@gmail.com>
2025-01-27 17:30:55 +0100

    first ver

--- tester.c
+++ tester.c
@@ -3,8 +3,12 @@
 extern float expo(float a, float b);
 
 int main(int argc, char** argv){
+    float base, exp;
     printf("hello rofl\n");
-    float num = 48.4f;
-    printf("Num:  %f, pow: %f \n",num, expo(num, 15.6f));
+    printf("enter base: ");
+    scanf("%f", &base);
+    printf("enter power: ");
+    scanf("%f", &exp);
+    printf("Num:  %f, exp: %f, pow: %f \n",base, exp, expo(base, exp));
     return 0;
 }
